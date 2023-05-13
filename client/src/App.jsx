cb112b93995d52913535ded81aac563d47d7217a
Arnas Verpečinskas <arnas.verpecinskas@unity3d.com>
2023-05-13 21:41:43 +0300

    Finalize frontend login with google

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './views/Landing';
 import AdminLanding from './views/admin/AdminLanding';
-import rootReducer from './reducers/rootReducer';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 
 function App() {
-    const store = createStore(rootReducer);
-
     return (
         <>
-            <Provider store={store}>
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<Landing />} />
-                        <Route path="/admin" element={<AdminLanding />} />
-                    </Routes>
-                </Router>
-            </Provider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/admin" element={<AdminLanding />} />
+                </Routes>
+            </Router>
         </>
     );
 }
--- client/src/views/admin/AdminLanding.jsx
+++ client/src/views/admin/AdminLanding.jsx
@@ -1,60 +1,83 @@
 import React from 'react';
-import { Button, Container } from '@mui/material';
+import { Alert, Button, Container } from '@mui/material';
 import { GoogleLogin } from '@react-oauth/google';
-import { useDispatch, useSelector } from 'react-redux';
 
 export default function AdminLanding() {
-    const dispatch = useDispatch();
-
     const [isAuthConfirmationInProgress, setAuthConfirmationInProgress] = React.useState(true);
-    // eslint-disable-next-line no-unused-vars
-    const authKey = useSelector((state) => state.authentication.authKey);
-    const [user, setUser] = React.useState(null);
+    const [authKey, setAuthKey] = React.useState(null);
+    const [error, setError] = React.useState('');
+
+    React.useEffect(() => {
+        const authKey = localStorage.getItem('authKey');
+        if (authKey) {
+            setAuthKey(authKey);
+        }
+    }, []);
 
     React.useEffect(() => {
         setAuthConfirmationInProgress(true);
-        if (authKey) {
-            fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${authKey}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    if (data.error) {
-                        dispatch({ type: 'CLEAR_AUTH_KEY' });
-                        return;
-                    }
-                    setUser(data);
-                })
-                .catch((error) => {
-                    dispatch({ type: 'CLEAR_AUTH_KEY' });
-                    console.log(error);
-                })
-                .finally(() => {
-                    setAuthConfirmationInProgress(false);
-                });
+        if (!authKey) {
+            setAuthConfirmationInProgress(false);
             return;
         }
-        setAuthConfirmationInProgress(false);
+
+        const verifyPermissions = async () => {
+            console.log(`Bearer ${authKey}`);
+            const response = await fetch('http://localhost:5000/user/haspermission?permission=items.see', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${authKey}`,
+                },
+            });
+            const hasAccess = await response.json();
+            if (!hasAccess) {
+                setAuthKey(null);
+                setError('You do not have permission to access this page');
+            }
+            setAuthConfirmationInProgress(false);
+        };
+        verifyPermissions();
     }, [authKey]);
 
     if (isAuthConfirmationInProgress) return <Container>Authenticating...</Container>;
-    if (!user && !isAuthConfirmationInProgress)
+
+    if (!authKey && !isAuthConfirmationInProgress)
         return (
-            <GoogleLogin
-                onSuccess={(credentialResponse) => {
-                    console.log(credentialResponse);
-                    setUser(credentialResponse);
-                    dispatch({ type: 'SET_AUTH_TOKEN', payload: { authKey: credentialResponse.credential } });
-                    setAuthConfirmationInProgress(false);
-                }}
-                onError={() => {
-                    console.log('Login Failed');
-                }}
-            />
+            <>
+                {error.length > 0 && (
+                    <Alert severity="error">
+                        {error}
+                        <br />
+                        Please login to continue
+                    </Alert>
+                )}
+                <GoogleLogin
+                    onSuccess={(credentialResponse) => {
+                        console.log(credentialResponse);
+                        localStorage.setItem('authKey', credentialResponse.credential);
+                        setAuthKey(credentialResponse.credential);
+                        setAuthConfirmationInProgress(false);
+                        setError('');
+                    }}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}
+                />
+            </>
         );
 
     return (
         <Container>
+            {error.length > 0 && <Alert severity="error">{error}</Alert>}
             <h1>Admin Landing Page</h1>
-            <Button onClick={() => setUser(null)}>Logout</Button>
+            <Button
+                onClick={() => {
+                    localStorage.removeItem('authKey');
+                    setAuthKey(null);
+                }}
+            >
+                Logout
+            </Button>
         </Container>
     );
 }
