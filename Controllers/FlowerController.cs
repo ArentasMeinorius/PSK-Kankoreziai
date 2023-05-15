7d95a7a94f42ea9fa6895dfa2aa9e3be26b848ee
arentasAtlantis <47771813+ArentasMeinorius@users.noreply.github.com>
2023-05-15 09:34:03 +0300

    flower controller with all methods

--- Controllers/FlowerController.cs
+++ Controllers/FlowerController.cs
@@ -17,16 +17,74 @@ public class FlowerController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Flower> Get()
+    [ProducesResponseType(typeof(List<Flower>), StatusCodes.Status200OK)]
+    [Produces("application/json")]
+    public IActionResult GetAll()
     {
-        return _context.Flowers.ToList();
+        return Ok(_context.Flowers.ToList());
+    }
+
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(Flower), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Produces("application/json")]
+    public async Task<IActionResult> Get(Guid id)
+    {
+        var flower = await _context.Flowers.FindAsync(id);
+        if (flower == null)
+        {
+            return StatusCode(404);
+        }
+        return Ok(flower);
     }
 
     [HttpPost]
-    public void Post(Flower myEntity)
+    [ProducesResponseType(typeof(Flower), StatusCodes.Status200OK)]
+    [Produces("application/json")]
+    public async Task<IActionResult> Post(FlowerDto newEntity)
     {
-        _context.Flowers.Add(myEntity);
-        _context.SaveChanges();
+        var newFlower = new Flower(Guid.NewGuid(), newEntity.Name, newEntity.Price);
+        _context.Flowers.Add(newFlower);
+        await _context.SaveChangesAsync();
+        return Ok(newFlower);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(typeof(Flower), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Produces("application/json")]
+    public async Task<IActionResult> Put(Guid id, FlowerDto newEntity)
+    {
+        var oldFlower = await _context.Flowers.FindAsync(id);
+        if (oldFlower == null)
+        {
+            return StatusCode(404);
+        }
+        var changedFlower = oldFlower with
+        {
+            Name = newEntity.Name,
+            Price = newEntity.Price
+        };
+        _context.Flowers.Remove(oldFlower);
+        _context.Flowers.Add(changedFlower);
+        await _context.SaveChangesAsync();
+        return Ok(changedFlower);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(typeof(Guid), StatusCodes.Status200OK)]
+    [Produces("application/json")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var oldFlower = await _context.Flowers.FindAsync(id);
+        if (oldFlower == null)
+        {
+            return Ok(id);
+        }
+        _context.Flowers.Remove(oldFlower);
+        await _context.SaveChangesAsync();
+        return Ok(id);
     }
 }
 
