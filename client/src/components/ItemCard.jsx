dc266b72e8a1dcddd24e1b50408dc2d8f90e7427
godagin <76496156+godagin@users.noreply.github.com>
2023-05-21 19:36:20 +0300

    Landing view implementation (#5)
    * Fix packages and unused SPA from react template
    
    * Change default app settings
    
    * Add google login backend verification
    
    * Finalize frontend login with google
    
    * Fix cherry-pick issues
    
    * flower controller with all methods
    
    * add landing page with mock data
    
    * update package-lock
    
    * add fetch implementation for landing page
    
    * fix overriden import
    
    * remove console log
    
    * rename product to be more generic
    
    * update href
    
    * update fetch link
    
    * remove uneccessary files
    
    * remove : in link, update package.json
    
    ---------
    
    Co-authored-by: Arnas Verpečinskas <arnas.verpecinskas@unity3d.com>
    Co-authored-by: arentasAtlantis <47771813+ArentasMeinorius@users.noreply.github.com>

---
+++ client/src/components/ItemCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Link, Typography } from '@mui/material';
+
+const ItemCard = ({ product }) => {
+    const getMoneyFromCents = (cents) => {
+        return cents / 100;
+    };
+
+    return (
+        <Link href={`/item/${product.id}`} variant="body1" underline="none">
+            <Box
+                component="img"
+                src={product.pictures.thumbnail}
+                sx={{
+                    width: '85%',
+                }}
+            ></Box>
+            <Typography variant="subtitle1" noWrap={true}>
+                {product.name}
+            </Typography>
+            <Typography variant="body1">Price: {getMoneyFromCents(product.price.cents)} €</Typography>
+        </Link>
+    );
+};
+
+ItemCard.propTypes = {
+    product: PropTypes.object,
+};
+
+export default ItemCard;
